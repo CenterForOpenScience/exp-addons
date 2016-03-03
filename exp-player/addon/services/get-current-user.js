41deb2a55d9a347e51da78eadab933ff0ec89487
Sam Chrisinger <s.chrisinger@gmail.com>
2016-03-03 10:55:36 -0500

    Add JIRA ticket for todo

--- exp-player/addon/services/get-current-user.js
+++ exp-player/addon/services/get-current-user.js
@@ -4,6 +4,7 @@ export default Ember.Service.extend({
     store: Ember.inject.service(),
     load: function() {
         // TODO work for real
+        // https://openscience.atlassian.net/browse/LEI-112
         return this.get('store'). findRecord('account', 'experimenter.accounts.tester0').then((account) => {
             return [account, account.get('profiles')[0]];
         });
